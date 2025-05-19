c88d606f6355d18dd4bcba9f5f4f4221c01f45f8
asadd2723 <asadu3040@gmail.com>
2025-05-19 09:54:41 +0500

    check thisdd

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+const root = {
+  hello: () => 'Hello from GraphQL!',
+};
+
+const app = express();
+app.use('/graphql', graphqlHTTP({ schema, rootValue: root, graphiql: true }));
+
+app.listen(4000, () => {
+  console.log('Server running at http://localhost:4000/graphql');
+});
