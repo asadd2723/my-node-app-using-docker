9daf7d2ab86c0e0dfc57f18926722b433e6bffae
asadd2723 <asadu3040@gmail.com>
2025-05-23 22:09:43 +0500

    check this

--- src/index.js
+++ src/index.js
@@ -16,5 +16,5 @@ const app = express();
 app.use('/graphql', graphqlHTTP({ schema, rootValue: root, graphiql: true }));
 
 app.listen(4000, () => {
-  console.log('Server running at http://localhost:4000/graphql');
+  console.log('Server running at....... http://localhost:4000/graphql');
 });
